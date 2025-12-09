83d06d824b38e9cae73a2b310a0941c4689650bb
Ryztard <Ryztad@live.com>
2025-12-09 22:01:04 +0100

    Test

--- WarehouseDevOps/next-app/pages/index.js
+++ WarehouseDevOps/next-app/pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>Warehouse Management System (Next.js)</h1>
-      <p>This is a basic Next.js placeholder. big test</p>
+      <p>This is a basic Next.js placeholder. big testing</p>
     </div>
   );
 }
